9a424ce96d4b6ac7316460d2fb04e94d406505a1
Alexey Kohanovsky <awfj837@gmail.com>
2025-04-02 00:42:02 +0300

    update nav

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -62,37 +62,32 @@ const Navbar = () => {
       <nav className="navbar z-50">
         {/* LOGO */}
         <Link to="/">
-          <img
-            src={logo}
-            alt="logo"
-            className="flex-none w-10"
-          />
+          <img src={logo} alt="logo" className="flex-none w-10" />
         </Link>
 
-        {/* SEARCH BOX */}
         <div
           className={
-            " absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
+            "absolute bg-white w-full left-0 top-full border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
             (searchBoxVisibility ? "show" : "hide")
           }
         >
           <input
             type="text"
             placeholder="Search"
-            className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+            className="interactivity-border w-full md:w-auto p-4 pl-6 pr-[12%] rounded-full placeholder:text-dark-grey md:pr-12"
             onKeyDown={handleChange}
           />
 
-          <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+          <i className="fi fi-rr-search flex-center absolute right-[8%] md:pointer-events-none top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
         </div>
 
         <div className="flex items-center gap-3 md:gap-6 ml-auto ">
           {/* SEARCH BUTTON */}
           <button
-            className="md:hidden  bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+            className="md:hidden flex-center interactivity icon rounded-full"
             onClick={() => setSearchBoxVisibility((currentVal) => !currentVal)}
           >
-            <i className="fi fi-rr-search text-xl"></i>
+            <i className="fi fi-ts-search-heart text-3xl flex-center"></i>
           </button>
 
           {/* NEW POST */}
@@ -104,14 +99,16 @@ const Navbar = () => {
           )}
 
           {/* THEME CHANGE */}
-          <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10 ">
+          <button
+            className="flex-center interactivity icon rounded-full"
+            onClick={changeChage}
+          >
             <i
               className={
-                "fi fi-rr-" +
-                (theme == "light" ? "moon-stars" : "sun") +
-                " text-2xl block mt-1"
+                "flex-center p-1 fi fi-tr-" +
+                (theme == "light" ? "moon-stars" : "brightness") +
+                " text-3xl"
               }
-              onClick={changeChage}
             ></i>
           </button>
 
